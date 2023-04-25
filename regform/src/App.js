204fb9fb8ff09042dc4862339156c307b3e31e00
Manish Kumar <mkdestiny2410@gmail.com>
2023-04-25 22:33:45 +0530

    first commit

---
+++ regform/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Form from "./components/Form";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Form />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
